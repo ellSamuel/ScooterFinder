0bff86230df2a482c9e87315e43d0bafc5d44953
Samuel Kebis <samokebis@gmail.com>
2022-05-05 17:32:08 +0200

    Add initial project

---
+++ ScooterFinder/ScooterFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScooterFinder
+//
+//  Created by Samuel Kebis on 05/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
